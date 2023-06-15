44bf5496a2585d0d38b6e76ebb322cacc11cba0e
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-16 00:55:31 +0700

    Add panzoom & tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
 	import Map from '../components/Map.svelte';
 </script>
 
-<h1>Open Alcohol Map</h1>
-
 <!-- <svelte:component this={SVG} /> -->
-<Map />
+<main class="bg-blue-200 h-[100svh]">
+	<h1>Open Alcohol Map</h1>
+	<Map />
+</main>
