91b76b794988f81682a17933c961185f2668beb7
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-16 00:32:13 +0700

    Add svg map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Map from '../components/Map.svelte';
+</script>
+
+<h1>Open Alcohol Map</h1>
+
+<!-- <svelte:component this={SVG} /> -->
+<Map />
